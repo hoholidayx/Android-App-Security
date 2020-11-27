8e1ddf110576ed87b827632430c1029608f0e9c6
hezhipeng <hoholiday@hotmail.com>
2020-11-27 16:24:22 +0800

    增加AppSecurityException的构造函数

--- appsecurity/src/main/java/hoholiday/app/lib/appsecurity/exception/AppSecurityException.java
+++ appsecurity/src/main/java/hoholiday/app/lib/appsecurity/exception/AppSecurityException.java
@@ -5,4 +5,18 @@ import androidx.annotation.Keep;
 @Keep
 public class AppSecurityException extends java.lang.SecurityException {
 
+    public AppSecurityException() {
+    }
+
+    public AppSecurityException(String s) {
+        super(s);
+    }
+
+    public AppSecurityException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public AppSecurityException(Throwable cause) {
+        super(cause);
+    }
 }
